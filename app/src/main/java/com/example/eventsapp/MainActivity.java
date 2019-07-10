794e977e5d5f2e36a318c918faa7d80e96ed210d
Ionut <alexandru.plosnita@student.usv.ro>
2019-07-10 12:55:42 +0300

    H2

--- app/src/main/java/com/example/eventsapp/MainActivity.java
+++ app/src/main/java/com/example/eventsapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.eventsapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity1 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
