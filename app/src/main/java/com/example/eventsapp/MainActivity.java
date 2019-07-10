1fb3b73ffc40911af912f77f59e6c792637a96de
Ionut <alexandru.plosnita@student.usv.ro>
2019-07-10 09:16:18 +0300

    Add upcoming event layout

--- app/src/main/java/com/example/eventsapp/MainActivity.java
+++ app/src/main/java/com/example/eventsapp/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.filter_events);
+        setContentView(R.layout.upcoming_events_layout);
     }
 }
