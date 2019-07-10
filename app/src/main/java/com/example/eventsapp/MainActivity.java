ccf6b791a04ddc7b0172cefaca03ec2d79cc8c7c
Ionut <alexandru.plosnita@student.usv.ro>
2019-07-10 12:53:43 +0300

    Hope to remove a conflict

--- app/src/main/java/com/example/eventsapp/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.eventsapp;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
- 
-}
---
+++ app/src/main/java/com/example/eventsapp/MainActivity1.java
@@ -0,0 +1,13 @@
+package com.example.eventsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.upcoming_events_layout);
+    }
+}
