1df55533cd8eb8d99dd3d6a2b45dbbfe0d04e70c
Ionut <alexandru.plosnita@student.usv.ro>
2019-07-10 12:49:44 +0300

    Eliminate onCreate

--- app/src/main/java/com/example/eventsapp/MainActivity.java
+++ app/src/main/java/com/example/eventsapp/MainActivity.java
@@ -5,9 +5,5 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-       // setContentView(R.layout.upcoming_events_layout);
-    }
+ 
 }
