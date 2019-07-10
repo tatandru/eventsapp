779ab32f136b65b050e0ce4d7c966d015c367300
Ionut <alexandru.plosnita@student.usv.ro>
2019-07-10 13:04:25 +0300

    Change layout

--- app/src/main/java/com/example/eventsapp/MainActivity.java
+++ app/src/main/java/com/example/eventsapp/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.upcoming_events_layout);
+        setContentView(R.layout.activity_main);
     }
 }
