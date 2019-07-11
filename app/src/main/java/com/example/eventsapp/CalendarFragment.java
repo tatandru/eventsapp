1076f669256dd3d5e4f1b3e1b961aa6ab62a1751
bogdanivascu <ivascubogdan97@gmail.com>
2019-07-11 10:42:59 +0300

    Remove CalendarActivity + add CalendarFragment

--- app/src/main/java/com/example/eventsapp/CalendarFragment.java
+++ app/src/main/java/com/example/eventsapp/CalendarFragment.java
@@ -4,15 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class CalendarFragment extends Fragment {
+
+    private CalendarView calendarView;
+    private TextView txtCurrentDate;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.calendar_activity,container,false);
+
+
+        View view = inflater.inflate(R.layout.calendar_activity, container, false);
+        calendarView = view.findViewById(R.id.calendarView);
+        txtCurrentDate = view.findViewById(R.id.txt_currentDay);
+        setCurrentDate();
+        setCalendarClickListener();
+        return view;
+    }
+
+    
+    /**
+     * My functions
+     */
+
+    private void setCurrentDate() {
+        Long l = calendarView.getDate();
+        String dateString = new SimpleDateFormat("cccc, LLLL d").format(new Date(l));
+        txtCurrentDate.setText(dateString);
+    }
+
+    private void setCalendarClickListener() {
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                //todo: show information of the event , if there is one
+            }
+        });
     }
 }
