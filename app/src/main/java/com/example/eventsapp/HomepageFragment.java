17887d1b0888ac65df5aaf024549f998ffbb4977
bogdanivascu <ivascubogdan97@gmail.com>
2019-07-11 12:20:59 +0300

    Add categories list + urlList

--- app/src/main/java/com/example/eventsapp/HomepageFragment.java
+++ app/src/main/java/com/example/eventsapp/HomepageFragment.java
@@ -13,6 +13,9 @@ import com.example.eventsapp.retrofitAPI.ApiConstans;
 import com.example.eventsapp.retrofitAPI.BaseRouteEvents;
 import com.example.eventsapp.retrofitAPI.RetrofitClient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -44,13 +47,36 @@ public class HomepageFragment extends Fragment {
             public void onResponse(Call<BaseRouteEvents> call, Response<BaseRouteEvents> response) {
 
                 baseRouteEventsBody = response.body();
+                int eventListSize = baseRouteEventsBody.getEmbedded().getEventList().size();
+                List<String> eventClassificationList = new ArrayList<>();
+                List<List<String>> urlList = new ArrayList<>();
 
-                for (int i = 0; i < baseRouteEventsBody.getEmbedded().getEventList().size(); i++) {
-                    System.out.println("------>" +
-                            baseRouteEventsBody.getEmbedded().getEventList().get(i));
-                    System.out.println();
 
+                for (int i = 0; i < eventListSize; i++) {
+
+                    int classificationListSize = baseRouteEventsBody.getEmbedded().getEventList().get(i).getClassficationList().size();
+                    int imageListSize = baseRouteEventsBody.getEmbedded().getEventList().get(i).getImgList().size();
+                    List<String> imageList = new ArrayList<>();
+                    System.out.println("------>" + baseRouteEventsBody.getEmbedded().getEventList().get(i));
+
+
+                    for (int j = 0; j < classificationListSize; j++) {
+                        String classificationName = baseRouteEventsBody.getEmbedded().getEventList().get(i).getClassficationList().get(j).getSegment().getEventType();
+                        if (!eventClassificationList.contains(classificationName)) {
+                            eventClassificationList.add(classificationName);
+                        }
+                    }
+
+
+                    for (int j = 0; j < imageListSize; j++) {
+                        String url = baseRouteEventsBody.getEmbedded().getEventList().get(i).getImgList().get(j).getImageURL();
+                        imageList.add(url);
+                    }
+                    urlList.add(imageList);
                 }
+                System.out.println(eventClassificationList.toString());
+                System.out.println(urlList.toString());
+
 
             }
 
