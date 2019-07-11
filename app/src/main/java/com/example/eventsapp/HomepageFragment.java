84c5c7c85714c5091da6c1623c5c03dc49041e3a
Ionut <alexandru.plosnita@student.usv.ro>
2019-07-11 17:18:25 +0300

    Solve conflict 1,5

--- app/src/main/java/com/example/eventsapp/HomepageFragment.java
+++ app/src/main/java/com/example/eventsapp/HomepageFragment.java
@@ -2,34 +2,28 @@ package com.example.eventsapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.eventsapp.adapters.CategoriesRVAdapter;
-import com.example.eventsapp.adapters.ItemRecycleViewAdapter;
 import com.example.eventsapp.retrofitAPI.ApiConstans;
 import com.example.eventsapp.retrofitAPI.BaseRouteEvents;
 import com.example.eventsapp.retrofitAPI.RetrofitClient;
-import com.google.android.gms.common.api.Status;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.model.TypeFilter;
 import com.google.android.libraries.places.api.net.PlacesClient;
-import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import java.util.Arrays;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import retrofit2.Call;
@@ -62,7 +56,7 @@ public class HomepageFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        //setupSearch();
+        setupSearch();
     }
 
     /**
@@ -119,7 +113,7 @@ public class HomepageFragment extends Fragment {
         PlacesClient placesClient = Places.createClient(getActivity());
     }
     private void setupList() {
-      //  layoutManager = new LinearLayoutManager(getContext());  // use a linear layout manager vertical
+        //  layoutManager = new LinearLayoutManager(getContext());  // use a linear layout manager vertical
         layoutManager = new GridLayoutManager(getContext(),2);  // use a grid layout manager with 2 columns
         rvItems.setLayoutManager(layoutManager);
 
@@ -130,12 +124,12 @@ public class HomepageFragment extends Fragment {
             @Override
             public void onClick(String os) {
 
-              try {
-                  getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UpcomingEventsFragment()).commit();
-              }catch (Exception e)
-              { e.printStackTrace();
-                  Toast.makeText(getContext(), os, Toast.LENGTH_SHORT).show();
-              }}
+                try {
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UpcomingEventsFragment()).commit();
+                }catch (Exception e)
+                { e.printStackTrace();
+                    Toast.makeText(getContext(), os, Toast.LENGTH_SHORT).show();
+                }}
         });
         rvItems.setAdapter(adapter);
 
@@ -164,4 +158,24 @@ public class HomepageFragment extends Fragment {
             dataSet.add(c);
         }
     }
+    private void setupSearch() {
+        //Todo:de refacut cu intent https://developers.google.com/places/android-sdk/autocomplete#option_2_use_an_intent_to_launch_the_autocomplete_activity
+        final int AUTOCOMPLETE_REQUEST_CODE = 1;
+        String placesApiKey = "f1d8dfdc4eecf6a683e9e0f11e8cc309";
+        Places.initialize(this.getActivity(), placesApiKey);
+        PlacesClient placesClient = Places.createClient(this.getActivity());
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+        final Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.FULLSCREEN, fields)
+                .setTypeFilter(TypeFilter.CITIES)
+                .build(this.getActivity());
+        EditText searchBar = getView().findViewById(R.id.et_search_bar);
+        searchBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+            }
+        });
+    }
+
 }
