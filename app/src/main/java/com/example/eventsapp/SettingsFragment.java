2579b325b21f6292a0be0c0d21655eef91e03390
Ionut <alexandru.plosnita@student.usv.ro>
2019-07-12 09:53:55 +0300

    Add on logo click handler

--- app/src/main/java/com/example/eventsapp/SettingsFragment.java
+++ app/src/main/java/com/example/eventsapp/SettingsFragment.java
@@ -4,15 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.eventsapp.retrofitAPI.Image;
+
 public class SettingsFragment extends Fragment {
+    private ImageView imageView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.settings,container,false);
+        View view= inflater.inflate(R.layout.settings,container,false);
+        imageView = (ImageView) view.findViewById(R.id.img_x_logout);
+        imageView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+               getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomepageFragment()).commit();
+            }
+        });
+     return  view;
     }
 }
