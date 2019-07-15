7b9dfb0cc05f631931dfdb4326e2f9790d12941b
Ionut <alexandru.plosnita@student.usv.ro>
2019-07-15 13:57:59 +0300

    Get data from homeFragment

--- app/src/main/java/com/example/eventsapp/UpcomingEventsFragment.java
+++ app/src/main/java/com/example/eventsapp/UpcomingEventsFragment.java
@@ -1,33 +1,57 @@
 package com.example.eventsapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.eventsapp.R;
+import com.example.eventsapp.adapters.CategoriesRVAdapter;
+import com.example.eventsapp.adapters.EventsListRVAdapter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class UpcomingEventsFragment extends Fragment {
     private ImageView imageView;
+    private RecyclerView rvItems;
+    private EventsListRVAdapter adapter;
     private TextView tvTitle;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<String> eventTitleDataSet;
+    private List<String> urlDateSet;
+    private List<String> urlRetrieveFromServer;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.upcoming_events_layout,container,false);
         tvTitle=view.findViewById(R.id.tv_title_upcoming_events);
+        rvItems=view.findViewById(R.id.rv_events_list);
+
         Bundle bundle=getArguments();
 
         if(bundle!=null) {
-            System.out.println(bundle.getString("title"));
-
+            urlRetrieveFromServer=new ArrayList<>();
+            urlRetrieveFromServer=bundle.getStringArrayList("img");
+            System.out.println(urlRetrieveFromServer);
             tvTitle.setText(String.valueOf(bundle.getString("title")));
+
         }System.out.println();
+        setupList();
         imageView = (ImageView) view.findViewById(R.id.img_filter_logo);
         imageView.setOnClickListener(new View.OnClickListener()
         {
@@ -35,9 +59,51 @@ public class UpcomingEventsFragment extends Fragment {
             public void onClick(View v)
             {
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FilterFragment()).commit();
+
             }
         });
         return view;
     }
+    private void setupList() {
+        layoutManager = new LinearLayoutManager(getContext());  // use a linear layout manager vertical
+        rvItems.setLayoutManager(layoutManager);
 
+        generateDataSet();
+
+        adapter = new EventsListRVAdapter(eventTitleDataSet, urlDateSet, getContext());
+        adapter.setCategoryClickListener(new EventsListRVAdapter.ItemClickListener() {
+            @Override
+            public void onClick(String os) {
+
+
+                try {
+                    System.out.println("WORKED" +
+                            "sadsadsadasdas" );
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getContext(), os, Toast.LENGTH_SHORT).show(); }
+            }
+        });
+        rvItems.setAdapter(adapter);
+
+    }
+
+
+    private void generateDataSet() {
+        eventTitleDataSet = new ArrayList<>();
+        urlDateSet = new ArrayList<>();
+        List<String>list=new ArrayList<>();
+        for(String e : urlRetrieveFromServer)
+        {
+            list.add("AAAAAAA");
+        }
+        eventTitleDataSet.addAll(list);
+        urlDateSet.addAll(urlRetrieveFromServer);
+
+
+
+
+        System.out.println(urlDateSet);
+
+    }
 }
