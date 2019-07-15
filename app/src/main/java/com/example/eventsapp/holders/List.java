3e02d1ed0d02c7ce9ea78914486ae709d5103d4f
Ionut <alexandru.plosnita@student.usv.ro>
2019-07-15 13:55:56 +0300

    Add holder for upcoming events

---
+++ app/src/main/java/com/example/eventsapp/holders/List.java
@@ -0,0 +1,28 @@
+package com.example.eventsapp.holders;
+
+public class List {
+
+    java.util.List<String> categories;
+    java.util.List<String> imagesURL;
+
+
+    public List() {
+        // ---
+    }
+
+    public java.util.List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(java.util.List<String> categories) {
+        this.categories = categories;
+    }
+
+    public java.util.List<String> getImagesURL() {
+        return imagesURL;
+    }
+
+    public void setImagesURL(java.util.List<String> imagesURL) {
+        this.imagesURL = imagesURL;
+    }
+}
