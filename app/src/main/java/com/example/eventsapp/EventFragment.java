8b23dd2ac3a3b30e8792d978a0a2e73ea0596293
Moraru Mihai-Alexandru <48557706+tatandru@users.noreply.github.com>
2019-07-18 14:10:38 +0300

    Update EventFragment.java

--- app/src/main/java/com/example/eventsapp/EventFragment.java
+++ app/src/main/java/com/example/eventsapp/EventFragment.java
@@ -1,31 +1,22 @@
 package com.example.eventsapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+
 
 import com.bumptech.glide.Glide;
 import com.example.eventsapp.database.EventsViewModel;
 import com.example.eventsapp.database.FavoriteEvents;
-
-import java.util.List;
-
 public class EventFragment extends Fragment {
 
     private TextView tv_event_name;
