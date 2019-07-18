860e67090cda0c84ea85c20cca642595cb40d378
Moraru Mihai-Alexandru <48557706+tatandru@users.noreply.github.com>
2019-07-18 14:45:28 +0300

    Update EventFragment.java

--- app/src/main/java/com/example/eventsapp/EventFragment.java
+++ app/src/main/java/com/example/eventsapp/EventFragment.java
@@ -17,33 +17,45 @@ import androidx.fragment.app.Fragment;
 import com.bumptech.glide.Glide;
 import com.example.eventsapp.database.EventsViewModel;
 import com.example.eventsapp.database.FavoriteEvents;
+import com.example.eventsapp.retrofitAPI.Event;
+
+import java.io.IOException;
+
 public class EventFragment extends Fragment {
 
     private TextView tv_event_name;
     private TextView tv_start_date;
     private TextView tv_end_date;
-    private ImageView eventImage;
     private ToggleButton favoriteButton;
-    private FavoriteEvents event;
+    private Event event;
     private EventsViewModel favoritesViewModel;
+    private ImageView eventImage;
+    private FavoriteEvents favoriteEvent;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.event_details, container, false);
-
+        Bundle bundle = getArguments();
+        try {
+            event = (Event) UpcomingEventsFragment.bytes2Object(bundle.getByteArray("event"));
+            System.out.println(event.toString());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         eventImage = view.findViewById(R.id.iv_event_image);
         tv_event_name = view.findViewById(R.id.tv_description);
         tv_start_date = view.findViewById(R.id.tv_start_data);
         favoriteButton = view.findViewById(R.id.btn_heart);
+        Glide.with(this.getContext()).load(event.getImgList().get(3).getImageURL()).into(eventImage);
+        tv_event_name.setText(event.getEventName());
+        tv_start_date.setText(event.getDates().getStartDate().toString());
         favoritesViewModel = new EventsViewModel(getActivity().getApplication());
-        Bundle bundle = getArguments();
-        if (bundle != null) {
-            Glide.with(this.getContext()).load(bundle.getString("imageEvent")).into(eventImage);
-            tv_event_name.setText(bundle.getString("title"));
-            tv_start_date.setText(bundle.getString("startDate"));
-        }
-        event = new FavoriteEvents(bundle.getString("title"), bundle.getString("imageEvent"), bundle.getString("startDate"));
+        favoriteEvent = new FavoriteEvents(event.getEventName(), event.getImgList().get(3).getImageURL(), event.getDates().getStartDate().getDayStartEvent());
         return view;
     }
 
@@ -54,7 +66,7 @@ public class EventFragment extends Fragment {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if (isChecked(favoriteButton))
-                    favoritesViewModel.insert(event);
+                    favoritesViewModel.insert(favoriteEvent);
             }
         });
     }
